6f067a98509f0d62826e18478e77b5ab886e06e4
Wagner Oliveira de Quadros <106637044+wagnerquadros@users.noreply.github.com>
2025-05-18 22:18:52 -0300

    Organizando as cores

--- app/src/main/java/com/wagner/simulapronaf/MainActivity.kt
+++ app/src/main/java/com/wagner/simulapronaf/MainActivity.kt
@@ -4,44 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.wagner.simulapronaf.ui.theme.SimulaPRONAFTheme
+import androidx.compose.ui.unit.dp
+import com.wagner.simulapronaf.ui.theme.FrameColor
+import com.wagner.simulapronaf.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SimulaPRONAFTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+
+
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    SimulaPRONAFTheme {
-        Greeting("Android")
-    }
-}
+fun ValorCArd(){}
+
+@Composable fun ParcelasCard() {}
+
+@Composable fun TaxaCard() {}
+
+@Composable fun BarraDeNevegacao(){}
+
